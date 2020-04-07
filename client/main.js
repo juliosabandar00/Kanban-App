6319096a6f3efc8a119be9c2c60c9d210354734f
Julio Sabandar <kjuliosabandar@gmail.comgit commit test.htmlgit push -u origin mastergit statusgit config --global user.email kjuliosabandar@gmail.com>
2020-04-08 00:41:44 +0700

    Google Login client side

---
+++ client/main.js
@@ -0,0 +1,24 @@
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
+
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method : 'POST',
+        url: 'http://localhost:5000/loginGoogle',
+        data : {
+            token: id_token
+        },
+        statusCode: {
+            200: function (response) {
+                localStorage.setItem('accessToken', response.accessToken);
+                checkLogIn();
+            }
+        }
+    })
+}
+  
