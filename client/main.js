ecb210c53ddf7ba166411c652cd2e79b255f84c7
Julio Sabandar <kjuliosabandar@gmail.comgit commit test.htmlgit push -u origin mastergit statusgit config --global user.email kjuliosabandar@gmail.com>
2020-04-08 19:50:06 +0700

    Can Now Display Tasks with Vue

--- client/main.js
+++ client/main.js
@@ -1,7 +1,34 @@
+let url = 'http://localhost:5000';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        tasks : null,
+        loggedIn: false,
+        logIn: false,
+        signUp: false,
+        kanban: true
+    },
+    created(){
+        axios({
+            url: url + '/task',
+            method: 'get',
+            headers:{
+                access_token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImVtYWlsIjoidXNlcjNAZ21haWwuY29tIiwiaWF0IjoxNTg2MzQ1Mjk1fQ.c1AsWQ4rm0lzFFB2mETlKzKl7C67stRQZHFFV5CedGc'
+            }
+        })
+        .then(response=>{
+            console.log(response)
+            this.tasks = response.data.tasks;
+        })
+    }
+  });
+  
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+        console.log('User signed out.');
     });
 }
 
